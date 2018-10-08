42997563a4e4bf722bc0f41a17f79624c3322ddd
Ilyes Hammemi <ilyeshammemi42@gmail.com>
2018-10-08 16:16:56 +0100

    8-10-2018 16:16

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-#include <rapidjson/document.h>
-#include <rapidjson/istreamwrapper.h>
+#include "rapidjson/document.h"
+#include "rapidjson/istreamwrapper.h"
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace rapidjson;
 using namespace std;
@@ -11,10 +13,50 @@ int main()
   ifstream ifs("questions.json");
   IStreamWrapper isw(ifs);
 
-  Document document;
-  document.parseStream(isw);
+  Document questions;
+  questions.ParseStream(isw);
 
-  cout << document[0];
+  vector<int> seenBefore;
+  int n = questions.Size();
+  int q;
+  string input;
+
+  cout << "we have " << n << " questions in questions.json !!" << endl;
+  cout << "Are you ready to play this game ??\n(if ready press 'y')\n";
+  cin >> input;
+
+  if (input != "y")
+    return 0;
+  else
+  {
+    while (true)
+    {
+      if (seenBefore.size() == n)
+      {
+        cout << "No more !!" << endl;
+        break;
+      }
+      do
+      {
+        q = rand() % n;
+      } while (find(seenBefore.begin(), seenBefore.end(), q) != seenBefore.end());
+      seenBefore.push_back(q);
+      cout << questions[q]["q"].GetString() << endl;
+      for (int i = 0; i < 3; i++)
+      {
+        cout << "(" << i + 1 << ") " << questions[q]["a"][i].GetString() << endl;
+      }
+      cin >> input;
+      if (input == questions[q]["c"].GetString())
+      {
+        cout << "That's correct !! Awesome !!!" << endl;
+      }
+      else
+      {
+        cout << "Not correct !! Not Awesome !!!" << endl;
+      }
+    }
+  }
 
   return 0;
 }
